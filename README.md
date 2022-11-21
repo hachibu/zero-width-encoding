dc446518d40ea3229c99f7ea77358c239309cb21
Raymond Sohn <raymondsohn@gmail.com>
2022-11-20 20:54:15 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# zero-width-encoding
-Zero-width text encoding and decoding
+# Zero-Width Encoding
+
+After reading [this article](https://www.ndtv.com/world-news/elon-musk-explains-how-tesla-caught-employee-leaking-data-3433802) on how Tesla caught an employee who leaked confidential data, I thought about how I would solve this problem.
+
+The approach outlined in the article has 2 problems.
+
+1. The encoding is easy to detect because it's visible to the naked eye.
+2. Identifying large amounts of people would require embedding a long unique signature into the email (e.g. random SHA256 hash) which would result in very long emails that would be very suspicious looking.
+
+This is a proof-of-concept for zero-width text encoding and decoding which could be used to identify data leaks by embedding a random SHA256 hash tied to the recipient in an email.
+
+## What is zero-width encoding?
+Zero-width encoding is a text-to-invisible-text encoding scheme. The core of the algorithm is very simple and can be summarized by the table below.
+
+| binary      | invisible character   |
+| ----------- | --------------------- |
+| 0           | zero-width space      |
+| 1           | zero-width joiner     |
+| end of byte | zero-width non-joiner |
+
+### Encoding
+UTF-8 string > Uint8 Array > binary string > invisible string
+
+### Decoding
+Invisible string > binary string > Uint8Array > UTF-8 string
+
+## Usage
+```
+node src/index.mjs
+```
---
+++ src/index.mjs
@@ -0,0 +1,65 @@
+import { TextEncoder, TextDecoder } from 'util';
+
+const ZERO_WIDTH_SPACE = '​'
+const ZERO_WIDTH_JOINER = '‍'
+const ZERO_WIDTH_NON_JOINER = '‌'
+
+export const ZeroWidthEncode = (string) => {
+    const encoder = new TextEncoder()
+    const byteEncoding = encoder.encode(string)
+
+    let binaryEncoding = []
+    for (const byte of byteEncoding) {
+        binaryEncoding.push(byte.toString(2))
+    }
+
+    let zeroWidthEncoding = ''
+    for (const binaryString of binaryEncoding) {
+        for (const digit of binaryString) {
+            zeroWidthEncoding += digit === '0' ? ZERO_WIDTH_SPACE : ZERO_WIDTH_JOINER
+        }
+        zeroWidthEncoding += ZERO_WIDTH_NON_JOINER
+    }
+
+    return zeroWidthEncoding
+}
+
+export const ZeroWidthDecode = (string) => {
+    let binaryEncoding = []
+    let currentBinaryString = ''
+    for (let i = 0; i < string.length; i++) {
+        const c = string[i]
+        if (c === ZERO_WIDTH_NON_JOINER) {
+            binaryEncoding.push(currentBinaryString)
+            currentBinaryString = ''
+            continue
+        }
+        currentBinaryString += c === ZERO_WIDTH_SPACE ? '0' : '1'
+    }
+
+    const integerEncoding = []
+    for (const binaryString of binaryEncoding) {
+        const n = parseInt(binaryString, 2)
+        integerEncoding.push(n)
+    }
+
+    const byteEncoding = new Uint8Array(integerEncoding)
+
+    const decoder = new TextDecoder('utf-8')
+    const stringEncoding = decoder.decode(byteEncoding)
+
+    return stringEncoding
+}
+
+function main() {
+    const str = "Hello World!"
+    const strEncoded = ZeroWidthEncode(str)
+    const strDecoded = ZeroWidthDecode(strEncoded)
+    console.log({
+        str,
+        strEncoded,
+        strDecoded
+    })
+}
+
+main()
